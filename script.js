a7e5d9f8a200afbd60f7fefb9dfa653a371a3b6b
AyushBodele <130462401+AyushBodele@users.noreply.github.com>
2023-09-21 20:12:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var t1 = gsap.timeline()
+
+
+t1.from(".nav",{
+    opacity:0,
+    delay:0.3
+})
+t1.from(".nav h1, .nav h4, .nav h3",{
+    y:-80,
+    duration:0.8,
+    delay:0.3,
+    opacity:0,
+    stagger:0.2
+})
+
+t1.from(".left h1",{
+    x:-500,
+    opacity:0,
+    duration:0.5,
+    stagger:0.4,
+    
+})
+
+t1.from(".right img",{
+   scale:0.5,
+   opacity:0,
+   duration:0.5
+})
+
+
+
+gsap.from("#page2 .box",{
+    scale:0,
+    opacity:0,
+    duration:1,
+    stagger:0.3,
+    scrollTrigger:{
+        trigger:"#page2 .box",
+        scroller:"body",
+    }
+})
